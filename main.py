4da4951ef94476713b1efacde85712200f2e24be
Hoshiiku <camilito2908mail.com>
2024-09-06 19:05:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+
+bot = commands.Bot(command_prefix = "/", intents = intents )
+
+
+
+@bot.event 
+async def on_ready():
+    print("Bot's online")
+
+
+
+
+rubbish_decomposition ={
+    "plastic bottle": "500 years",
+    "can": "10 years",
+    "plastic bag":"20 years",
+    "glass": "4000 years",
+    "paper": "6 weeks",
+    "diaper":"500 years" ,
+    "food": "2 weeks"
+}
+
+
+@bot.command()
+
+async def classify(ctx, *, object: str):
+    object = object.lower()
+    if object in rubbish_decomposition:
+        time = rubbish_decomposition[object]
+        await ctx.send(f"The object {object} takes {time} to decompose")
+    else:
+        await ctx.send("No data was found!")
+
+
+
+
+
+
+
+
+
+
+
+bot.run("TOKEN GOES HERE!")
+
